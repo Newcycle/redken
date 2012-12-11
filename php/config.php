6777dc0a5bd9c2e69f0b1791ba10c5e25cbe02d6
Newcycle <pancentric@newcycle.com.ar>
2012-12-11 20:54:49 -0300

    Final Change
    We still need to fix the share for Chrome. That would be the final
    change and your idea with the edition of the code.

---
+++ php/config.php
@@ -0,0 +1,21 @@
+<?php
+$appId = '392295790850040';
+$secret = 'dfa82bab3c4e0cee8656ee35244cb867';
+$pageid = '20240621615';
+$app_url = 'lorealredken';
+ 
+$canvas_page = 'http://www.facebook.com/pages/Newcycle-Lab/'.$pageid.'?sk=app_'.$appId;
+$apiroot = 'http://www.facebook.com/pages/Newcycle-Lab/'.$pageid.'?sk=app_'.$appId;
+$root = 'https://www.newcyclelabs.com.ar/tomasapp/redken';
+
+$canvas_supercut_page = 'http://www.facebook.com/pages/apptesteo/117276385074998?sk=app_453582368012227';
+$canvas_redken_page = 'http://www.facebook.com/pages/Newcycle-Lab/'.$pageid.'?sk=app_392295790850040';
+
+$render_pages['noFan'] = 'includes/no-fan.php';
+$render_pages['form'] = 'includes/form.php';
+$render_pages['thanks'] = 'includes/thanks.php';
+
+$render_page='';
+$redken_code = "HDMP6R";
+$supercut_code = "LXPFT1";
+?>
